3a05f56125f3920be7b859e345e59ee13124b1cc
Kyle <kgladfelder@gmail.com>
2022-08-17 20:12:57 -0500

    chore: temp commit

---
+++ tests/Home.spec.ts
@@ -0,0 +1,13 @@
+// import '@testing-library/jest-dom';
+
+// import { render, screen } from '@testing-library/svelte';
+
+// import Home from '../src/routes/index.svelte';
+
+// describe('should render the home page', () => {
+// 	it('shows proper heading when rendered', () => {
+// 		render(Home);
+// 		const heading = screen.getByText('Welcome to Game Recommender');
+// 		expect(heading).toBeInTheDocument();
+// 	});
+// });
