baff9b32f85ffe27f13dd49f7f1169a167d3187c
Kyle <kgladfelder@gmail.com>
2023-06-03 14:38:40 -0500

    chore: more formatting stuff

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,15 @@
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	parser: "@typescript-eslint/parser",
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+	plugins: ["svelte3", "@typescript-eslint"],
+	ignorePatterns: ["*.cjs"],
+	overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
 	settings: {
-		'svelte3/typescript': () => require('typescript'),
+		"svelte3/typescript": () => require("typescript"),
 	},
 	parserOptions: {
-		sourceType: 'module',
+		sourceType: "module",
 		ecmaVersion: 2020,
 	},
 	env: {
@@ -18,6 +18,6 @@ module.exports = {
 		node: true,
 	},
 	rules: {
-		quotes: ['warn', 'double'],
+		quotes: ["warn", "double"],
 	},
 };
