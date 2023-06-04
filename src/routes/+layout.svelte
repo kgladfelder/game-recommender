f5abdf98e6f7ea95e3d3bf242d54f9785066f201
Kyle <kgladfelder@gmail.com>
2023-06-04 10:08:32 -0500

    chore: switch to AppShell

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 	// Most of your app wide CSS should be put in this file
 	import "../app.postcss";
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from "@floating-ui/dom";
-	import { storePopup } from "@skeletonlabs/skeleton";
+	import { AppShell, storePopup } from "@skeletonlabs/skeleton";
 	import type { PageData } from "./$types";
 
 	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
@@ -23,11 +23,14 @@
 	<title>Rec Game</title>
 	<meta name="robots" content="noindex nofollow" />
 </svelte:head>
-
-<div class="flex flex-col h-screen">
-	<Header username="{username}" admin="{admin}" />
+<AppShell>
+	<svelte:fragment slot="header">
+		<Header username="{username}" admin="{admin}" />
+	</svelte:fragment>
 	<main class="flex-grow">
 		<slot />
 	</main>
-	<Footer />
-</div>
+	<svelte:fragment slot="footer">
+		<Footer />
+	</svelte:fragment>
+</AppShell>
